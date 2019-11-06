8c70a329b434f39b20fbfaca7796bb63112edc46
phillmac <phillmac@users.noreply.github.com>
2019-11-06 17:32:26 +1100

    Emit ready event on db ready

--- src/OrbitDB.js
+++ src/OrbitDB.js
@@ -414,6 +414,7 @@ class OrbitDB {
         logger.warn(`Not a valid OrbitDB address '${address}', creating the database`)
         options.overwrite = options.overwrite ? options.overwrite : true
         return this.create(address, options.type, options).then((db) => {
+          db.events.on('ready', (address, heads) => this.events.emit('ready', address, heads))
           this.events.emit('open', db.address.toString())
           return db
         })
@@ -453,6 +454,7 @@ class OrbitDB {
     // Open the the database
     options = Object.assign({}, options, { accessControllerAddress: manifest.accessController, meta: manifest.meta })
     return this._createStore(manifest.type, dbAddress, options).then((db) => {
+      db.events.on('ready', (address, heads) => this.events.emit('ready', address, heads))
       this.events.emit('open', db.address.toString())
       return db
     })
